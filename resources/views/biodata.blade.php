6464fc8f1a5834e672604081fc11ba1c28a73934
khairunnisaalallah <124749527+khairunnisaalallah@users.noreply.github.com>
2024-02-21 10:05:29 +0700

    add controller and view

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Biodata Diri </h1>
+    <h2> Hai teman teman perkenalkan saya Khairunnisa  </h2>
+    <h2> Jurusan Teknologi Informasi </h2>
+    <h2> Angkatan 2022 </h2>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\halloController;
+use App\Http\Controllers\biodataController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hallo',[halloController::class,'index']);
+Route::get('/biodata',[biodataController::class,'index']);
